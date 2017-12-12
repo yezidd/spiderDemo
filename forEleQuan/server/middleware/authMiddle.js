3af4533889b4b86ffee46ba236bb6b99aa63d5a8
yzdd <yjs10086>
2017-12-12 17:37:55 +0800

    bugfixed

--- forEleQuan/server/middleware/authMiddle.js
+++ forEleQuan/server/middleware/authMiddle.js
@@ -1,3 +1,9 @@
-export async function checkToken() {
+export async function checkToken(ctx, next) {
+  console.log(ctx.request.header.authorization);
+  if (ctx.request.header.authorization) {
+    await next();
+  } else {
+    ctx.throw (401, {}, "token 验证失败");
+  }
 
 }
